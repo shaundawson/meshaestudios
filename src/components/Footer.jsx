6642c58e8cf1ede4be5cac7f6514271ab41dfa9b
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2025-11-09 21:04:59 -0500

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
                 <span className={styles.footDivider}>|</span>
                 <a
                     className={styles.footerLink}
-                    href="#"
+                    href="https://www.meshaestudios.com"
                 >
                     privacy policy
                 </a>

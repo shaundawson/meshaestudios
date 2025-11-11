bece765135cc1d4ff61f08fb35d320504287058b
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2025-11-11 15:06:05 -0500

    design lab update

--- src/pages/Design_Lab.jsx
+++ src/pages/Design_Lab.jsx
@@ -3,10 +3,22 @@ import Gallery from '../components/Gallery'
 import styles from '../styles/Design_Lab.module.css'
 
 const Design_Lab = () => (
-    <div>
-        <div className={styles.galleryBG}>
-            <Gallery />
+    <div className={styles.galleryBG}>
+        <div className={styles.headerAndDesc}>
+            <h1 className={styles.designLabHeading}>
+                <span className={styles.headingNum}>03</span>
+                <span className={styles.headingMain}>design lab</span>
+            </h1>
+            <div className={styles.labDescription}>
+                <p>
+                    The Design Lab at Meshae Studios is where visual storytelling meets innovative technology.
+                    We create digital branding, culture-driven campaigns, and gallery showcases for justice-minded creatives, founders, and brands.
+                    Here you'll find projects that merge minimal Swiss structure, cinematic aesthetics, and purposeful strategy.
+                </p>
+            </div>
         </div>
+        {/* Important: DO NOT wrap Gallery in .masonryGallery here! <Gallery /> must do it internally */}
+        <Gallery />
     </div>
 )
 
